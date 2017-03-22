b546bf8d288bbd009c13126332aba0c15b4e2d2f
Quentin Roy <quentin@quentinroy.fr>
2017-03-22 15:44:23 +0800

    test: ADD_TODO action handing by todos reducer

--- src/reducers/index.spec.js
+++ src/reducers/index.spec.js
@@ -6,4 +6,15 @@ describe('todos reducer', () => {
     const initialState = []
     expect(todos(initialState, unknownAction)).toBe(initialState)
   })
+
+  it('creates a new todo on ADD_TODO actions', () => {
+    const action = { type: 'ADD_TODO', id: 42, text: 'whatever' }
+    const initialState = [
+      { id: 1, text: 'foo', completed: true }
+    ]
+    expect(todos(initialState, action)).toEqual([
+      { id: 1, text: 'foo', completed: true },
+      { id: 42, text: 'whatever', completed: false }
+    ])
+  })
 })
